6f1954e25a97c75cde58ad0072e84edfe3bf9f79
jtm <noreply@jtm.cx>
2026-08-11 12:14:30 -0400

    Consolidate fonts.css into style.css

--- app/Main.hs
+++ app/Main.hs
@@ -478,8 +478,6 @@ buildBaseHead = do
   meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1.0"]
   withRelative [absfile|/favicon.ico|] $ \path ->
     link_ [rel_ "icon", type_ "image/x-icon", href_ path]
-  withRelative [absfile|/static/fonts.css|] $ \path ->
-    link_ [rel_ "stylesheet", href_ path]
   withRelative [absfile|/static/style.css|] $ \path ->
     link_ [rel_ "stylesheet", href_ path]
 
